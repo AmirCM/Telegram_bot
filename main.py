1ca8e787044844ad5921eb176099e6b8e71cf2fb
AmirCM <amirbehzadfar.h@gmail.com>
2020-11-02 21:38:15 +0330

    class defenition

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.tgju.org/'
+dollar = 0
+eur = 1
+gbp = 2
 
 
 class Currency:
-    def __init__(self, table):
-        self.table = table
-        price = self.table.find('td', {'class': 'nf'}).text
-        price = price.split(',')
-        self.price = int(price[0])*100 + int(price[1])//10
+    def __init__(self):
+
+        self.url = ['https://www.tgju.org/']
+        self.soup = [None, None, None]
+        self.price = [0, 0, 0]
+        self.c_keys = ['price_dollar_rl', 'price_eur', 'price_gbp']
+
+    def update_db(self):
+        r = requests.get(self.url[0])
+        self.soup[0] = BeautifulSoup(r.text, features='lxml')
+        divs = self.soup[0].find_all('div', class_='home-fs-row')
+        d_in_d = []
+        for d in divs:
+            d_in_d += d.find_all('table', class_='data-table market-table dark-head market-section-right')
+        d_in_d = d_in_d[0]
+
+        for i in range(3):
+            price = d_in_d.find('tr', {"data-market-row": self.c_keys[i]}).find('td', {'class': 'nf'}).text.split(
+                ',')
+            self.price[i] = int(price[0]) * 100 + int(price[1]) // 10
 
 
 if __name__ == '__main__':
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features='lxml')
-
-    divs = soup.find_all('div', class_='home-fs-row')
-    d_in_d = []
-    for d in divs:
-        d_in_d += d.find_all('table', class_='data-table market-table dark-head market-section-right')
-    d_in_d = d_in_d[0]
-
-    dollar = Currency(d_in_d.find('tr', {"data-market-row": "price_dollar_rl"}))
-    eur = Currency(d_in_d.find('tr', {"data-market-row": "price_eur"}))
-    gbp = Currency(d_in_d.find('tr', {"data-market-row": "price_gbp"}))
-
-    print(dollar.price, eur.price, gbp.price)
+    c = Currency()
+    c.update_db()
+    print(c.price)
